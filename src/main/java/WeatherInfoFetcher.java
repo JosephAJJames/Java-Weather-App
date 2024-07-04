8235aa2c9ad82ad50037945a96ecd14e0421e7e4
jamesj9@lancaster.ac.uk <jamesj9@lancaster.ac.uk>
2024-07-04 14:50:34 +0100

    Added plugin to pom.xml to generate .jar with dependencies.

--- src/main/java/WeatherInfoFetcher.java
+++ src/main/java/WeatherInfoFetcher.java
@@ -1,2 +1,9 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class WeatherInfoFetcher {
+    private OkHttpClient client = new OkHttpClient();
+
+    public WeatherInfoFetcher() {}
 }
