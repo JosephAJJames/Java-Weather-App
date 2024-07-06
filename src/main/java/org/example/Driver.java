148d07a8cabf4f0d05d33dafe585beea010d4dd6
jamesj9@lancaster.ac.uk <jamesj9@lancaster.ac.uk>
2024-07-06 15:59:56 +0100

    Added .env file

--- src/main/java/org/example/Driver.java
+++ src/main/java/org/example/Driver.java
@@ -4,5 +4,7 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         WeatherInfoFetcher weatherFetcher = new WeatherInfoFetcher();
+        WeatherApp myApp = new WeatherApp(weatherFetcher);
+        myApp.run();
     }
 }
---
+++ src/main/java/org/example/WeatherApp.java
@@ -0,0 +1,64 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class WeatherApp {
+    private WeatherInfoFetcher fetcher;
+    private CityCoordinatesFetcher cityFetcher;
+
+    private int round;
+    public WeatherApp(WeatherInfoFetcher myFetcher) {
+        this.fetcher = myFetcher;
+    }
+
+    public void run() {
+        while (1 == 1) {
+            this.setRound(1);
+            this.printFirstRoundOptions();
+            this.processFirstRound();
+            this.setRound(2);
+            this.printSecondRoundOption();
+        }
+    }
+
+    public void printSecondRoundOption() {
+        System.out.println("Input your city:");
+    }
+
+    public void printFirstRoundOptions() {
+        System.out.println("1. Input 1 to select city");
+        System.out.println("2. Input 2 to exit");
+    }
+
+    public void setRound(int num) {
+        this.round = num;
+    }
+
+    public int getRound() {
+        return this.round;
+    }
+
+    public void processFirstRoundOptions(int option) {
+        if (option == 1) {
+            System.out.println("City Selected");
+        } else { //must be 2
+            System.out.println("Exiting.....");
+            System.exit(1);
+        }
+    }
+
+    public void processSecondRoundOptions(String city) {
+        
+    }
+
+    public void processFirstRound() {
+        Scanner myScanner = new Scanner(System.in);
+        int result = myScanner.nextInt();
+        this.processFirstRoundOptions(result);
+    }
+
+    public void processSecondRound() {
+        Scanner myScanner = new Scanner(System.in);
+        String result = myScanner.next();
+    }
+}
