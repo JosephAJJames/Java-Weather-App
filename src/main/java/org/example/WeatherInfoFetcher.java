8562cbeda3dfba7ac30dae59a1fb7cc3e608c4c4
jamesj9@lancaster.ac.uk <jamesj9@lancaster.ac.uk>
2024-07-07 23:52:22 +0100

    Making a new object to represent weather json

--- src/main/java/org/example/WeatherInfoFetcher.java
+++ src/main/java/org/example/WeatherInfoFetcher.java
@@ -7,8 +7,29 @@ import okhttp3.Response;
 public class WeatherInfoFetcher {
     private OkHttpClient client = new OkHttpClient();
     private String apiKey;
+    private CityCoordinatesFetcher coordinatesFetcher;
 
     public WeatherInfoFetcher(String apiKey) {
         this.apiKey = apiKey;
+        this.coordinatesFetcher = new CityCoordinatesFetcher(this.apiKey);
     }
+
+    public void makeRequest(String cityName) {
+        TupleOfDouble CoOrdinates = this.coordinatesFetcher.convertCityNameToCoOrdinates(cityName);
+        CoOrdinates.print();
+        Request req = new Request.Builder().url(
+                        String.format("https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s", CoOrdinates.getFirst(), CoOrdinates.getSecond(), this.apiKey))
+                .build();
+        try {
+            Response res = client.newCall(req).execute();
+
+
+
+
+
+        } catch (Exception e) {
+            System.out.println("Heavy");
+        }
+    }
+
 }
