e36faf5b41320bb943cbaeead4684b409fe799ee
jamesj9@lancaster.ac.uk <jamesj9@lancaster.ac.uk>
2024-07-07 23:21:16 +0100

    Finished custom tuple class.

--- src/main/java/org/example/TupleOfDouble.java
+++ src/main/java/org/example/TupleOfDouble.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public class TupleOfDouble {
+    private double first;
+    private double second;
+
+    public TupleOfDouble(double first, double second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public double getFirst() {
+        return this.first;
+    }
+
+    public double getSecond() {
+        return this.second;
+    }
 }
