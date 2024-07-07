44827061c1351a194107c9bf4051c9c9e3ee37e3
jamesj9@lancaster.ac.uk <jamesj9@lancaster.ac.uk>
2024-07-07 23:24:46 +0100

    Finished method

--- src/main/java/org/example/CityCoordinatesFetcher.java
+++ src/main/java/org/example/CityCoordinatesFetcher.java
@@ -1,5 +1,9 @@
 package org.example;
+import com.google.gson.Gson;
 
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.List;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -7,8 +11,30 @@ import okhttp3.Response;
 public class CityCoordinatesFetcher {
     private OkHttpClient client = new OkHttpClient();
     private String apiKey;
+    private Gson gson = new Gson();
 
     public CityCoordinatesFetcher(String apiKey) {
         this.apiKey = apiKey;
+    }
+
+    public TupleOfDouble convertCityNameToCoOrdinates (String cityName){
+        Request req = new Request.Builder().url(
+                String.format("http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=5&appid=%s", cityName, this.apiKey))
+                .build();
+
+        try {
+            Response res = client.newCall(req).execute();
+            String jsonData = res.body().string();
+            Type listType = new TypeToken<List<LocationObject>>() {}.getType();
+            List<LocationObject> locations = this.gson.fromJson(jsonData, listType);
+
+            LocationObject myObj =  locations.get(0);
+            TupleOfDouble tuple = new TupleOfDouble(myObj.getLat(), myObj.getLon());
+            return tuple;
+
+        } catch (Exception e) {
+            System.out.println("Request failed");
+        }
+        return null;
     }
 }
