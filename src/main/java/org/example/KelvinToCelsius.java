6ce714f97ab8d609a2ed7ef83f820ba7abc0b1f4
jamesj9@lancaster.ac.uk <jamesj9@lancaster.ac.uk>
2024-07-09 15:50:38 +0100

    Finished class

--- src/main/java/org/example/KelvinToCelsius.java
+++ src/main/java/org/example/KelvinToCelsius.java
@@ -1,4 +1,8 @@
 package org.example;
 
-public class KelvinToCelsius {
+public class KelvinToCelsius{
+
+    public int convert(Number kelvin) {
+        return (int) (kelvin.doubleValue() - 275.15);
+    }
 }
