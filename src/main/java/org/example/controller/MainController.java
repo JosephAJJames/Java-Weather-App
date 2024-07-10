bbdb49d4de59a7fb5d715969953ce46430461d11
jamesj9@lancaster.ac.uk <jamesj9@lancaster.ac.uk>
2024-07-10 13:20:40 +0100

    Added event listener to map in the controller

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -1,11 +1,28 @@
 package org.example.controller;
 
 import org.example.view.MainView;
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.input.PanMouseInputListener;
+
+import javax.swing.event.MouseInputListener;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 
 public class MainController {
     private MainView mainView;
 
     public MainController(MainView view) {
         this.mainView = view;
+        this.addEventListeners(view);
+    }
+
+    public void addEventListeners(MainView view) {
+        JXMapViewer map = this.mainView.getMap();
+
+        MouseInputListener mouseInputListener = new PanMouseInputListener(map);
+        map.addMouseListener(mouseInputListener);
+        map.addMouseMotionListener(mouseInputListener);
     }
 }
